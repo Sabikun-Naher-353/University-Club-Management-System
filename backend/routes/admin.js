f60be4675cf3d69819c1799840583b33ab33ca88
Sabikun Naher Islam Amrin <s.n.i.a00134@gmail.com>
2026-04-19 22:07:04 +0600

    Reorder university and club routes in admin.js

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -4,8 +4,6 @@ const router  = express.Router();
 const adminController = require("../controllers/adminController");
 
 router.post("/login",                       adminController.login);
-router.get("/universities",                 adminController.getUniversities);
-router.get("/clubs",                        adminController.getClubs);
 router.post("/check-email",                 adminController.checkEmail);
 router.post("/check-username",              adminController.checkUsername);
 router.post("/register",                    adminController.register);
@@ -14,6 +12,7 @@ router.post("/register",                    adminController.register);
 router.get("/stats",                        adminController.getStats);
 
 // UNIVERSITIES
+router.get("/universities",                 adminController.getUniversities);     
 router.get("/all-universities",             adminController.getAllUniversities);
 router.get("/pending-universities",         adminController.getPendingUniversities);
 router.put("/approve-university/:id",       adminController.approveUniversity);
@@ -21,7 +20,8 @@ router.delete("/reject-university/:id",     adminController.rejectUniversity);
 router.delete("/delete-university/:id",     adminController.deleteUniversity);
 
 // CLUBS
-router.get("/all-clubs",                    adminController.getAllClubs);
+router.get("/clubs",                        adminController.getAllClubs);           
+router.get("/all-clubs",                    adminController.getAllClubs);          
 router.get("/pending-clubs",                adminController.getPendingClubs);
 router.put("/approve-club/:id",             adminController.approveClub);
 router.delete("/delete-club/:id",           adminController.deleteClub);
